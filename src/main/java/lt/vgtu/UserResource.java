f9bda12c7f662aa561ae9f3db2437111c4219b9b
Aymeric Giraudet <neadj59@gmail.com>
2015-04-19 19:07:51 +0300

    Can now create and save a test in the db

--- src/main/java/lt/vgtu/UserResource.java
+++ src/main/java/lt/vgtu/UserResource.java
@@ -1,9 +1,12 @@
 package lt.vgtu;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import lt.vgtu.*;
 import com.mongodb.client.MongoCollection;
 import static lt.vgtu.App.db;
 import static com.mongodb.client.model.Filters.*;
+import com.mongodb.util.JSON;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +26,7 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import jdk.nashorn.internal.parser.JSONParser;
 import org.bson.Document;
 
 @Path("/user")
@@ -30,61 +34,11 @@ import org.bson.Document;
 public class UserResource {
 
     private static Map<Integer, User> users = new HashMap<>();
-
+    
     @POST
-    public User createUser(User user) {
-        int id = users.size();
-        user.setId(id + 1);
-        users.put(user.getId(), user);
-        return user;
-    }
-
-    @DELETE
-    @Path("{id}")
-    public Response deleteUser(@PathParam("id") Integer id) {
-        if (users.containsKey(id)) {
-            return Response.accepted().status(Status.ACCEPTED).build();
-        }
-        return Response.accepted().status(Status.NOT_FOUND).build();
-    }
-
-    protected User find(String name) {
-        User out = null;
-        for (User user : users.values()) {
-            if (user.getName().equals(name)) {
-                return user;
-            }
-        }
-        return out;
-    }
-
-    protected User find(int id) {
-        return users.get(id);
-    }
-
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("{id}")
-    public Response updateUser(@PathParam("id") int id,
-            User user) {
-        User oldUser = find(id);
-        System.out.println("Should update user with id: " + id
-                + " (" + oldUser + ") to " + user);
-        if (user == null) {
-            throw new WebApplicationException(404);
-        }
-        oldUser.setName(user.getName());
-        return Response.status(200).entity(oldUser).build();
-    }
-
-    @GET
-    @Path("/{name}")
-    public User getUser(@PathParam("name") String name) {
-        User out = find(name);
-        if (out == null) {
-            throw new WebApplicationException(404);
-        }
-        return out;
+    public String createTest(String json) {
+        db.getCollection("tests").insertOne(Document.parse(json));
+        return json;
     }
 
     @GET
--- src/main/webapp/all.js
+++ src/main/webapp/all.js
@@ -61,12 +61,56 @@ function afficheListUsers(data) {
     $("#reponse").html(html);
 }
 
-function addAnswer() {
-    var html = "<div class='input-group'><span class='input-group-addon'><input type='radio' name='0' value='1'></span><input type='text' class='form-control'></div><br>";
-    $(html).insertBefore("#0 button");
+function addAnswer(senderIndex) {
+    var nbAnswers = $("#" + senderIndex + " [type='radio']").length;
+    var html = "<div class='input-group'><span class='input-group-addon'><input type='radio' name='" + senderIndex + "' value='" + nbAnswers + "'></span><input type='text' class='form-control' name='" + nbAnswers + "'></div><br>";
+    $(html).insertBefore("#" + senderIndex + " button");
 }
 
+var nbQuestions = 0;
+
 function addQuestion() {
-    var html = "<div class='panel panel-default'><div class='panel-heading'><input type='text' class='form-control' placeholder='Question'></div><div class='panel-body' id='0'><div class='input-group'><span class='input-group-addon'><input type='radio' name='0' value='0' checked></span><input type='text' class='form-control'></div><br><div class='input-group'><span class='input-group-addon'><input type='radio' name='0' value='1'></span><input type='text' class='form-control'></div><br><button type='button' onclick='addAnswer()' class='btn btn-default btn-xs'>Add an answer</button></div></div>";
-    $(html).insertBefore("button:last")
+    ++nbQuestions;
+    var html = "<div class='panel panel-default' id='" + nbQuestions + "'><div class='panel-heading'><input type='text' class='form-control' placeholder='Question'></div><div class='panel-body'><div class='input-group'><span class='input-group-addon'><input type='radio' name='" + nbQuestions + "' value='0' checked></span><input type='text' class='form-control' name='" + 0 + "'></div><br><div class='input-group'><span class='input-group-addon'><input type='radio' name='" + nbQuestions + "' value='1'></span><input type='text' class='form-control' name='" + 1 + "'></div><br><button type='button' onclick='addAnswer(" + nbQuestions + ")' class='btn btn-default btn-xs'>Add an answer</button></div></div>";
+    $(html).insertBefore("#add")
+}
+
+var test = new Object();
+
+function saveToDB() {
+    test.subject = $("#info [placeholder='Class']").val();
+    test.title = $("#info [placeholder='Subject']").val();
+    test.teacher = $("#info [placeholder='Teacher']").val();
+    test.questions = [];
+    for (i = 0; i <= nbQuestions; i++) {
+        var quest = new Object();
+        var currentPanel = $("#" + i);
+        quest.text = currentPanel.find("[placeholder='Question']").val();
+        quest.responses = [];
+        for (j = 0; j < currentPanel.find("[type='radio']").length; j++) {
+            var radio = currentPanel.find("[type='radio'][value='" + j + "']");
+            var text = currentPanel.find("[type='text'][name='" + j + "']");
+            quest.responses.push(text.val());
+            if (radio.is(":checked"))
+                quest.answer = j;
+        }
+        test.questions.push(quest);
+    }
+    postTest(JSON.stringify(test));
+}
+
+function postTest(json) {
+    $.ajax({
+        type: 'POST',
+        contentType: 'application/json',
+        url: "rest/user/",
+        dataType: "json",
+        data: json,
+        success: function (data, textStatus, jqXHR) {
+            alert("success!");
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('postUser error: ' + textStatus);
+        }
+    });
 }
